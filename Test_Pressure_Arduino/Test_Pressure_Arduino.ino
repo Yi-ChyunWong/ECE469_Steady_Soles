da294e1ea05383c0851a987c232270fceeab89a9
Gordon Kim <gordonkimor@gmail.com>
2025-04-15 17:58:01 -0400

    Move algorithm

--- Test_Pressure_Arduino/Test_Pressure_Arduino.ino
+++ Test_Pressure_Arduino/Test_Pressure_Arduino.ino
@@ -6,6 +6,8 @@
 *
 **/
 
+
+#include <string.h>
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
@@ -13,17 +15,25 @@
 #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
-BLECharacteristic *pCharacteristic;
-BLEServer *pServer;
-
-bool deviceConnected = false;
-
+int v[8];
 const int motorFront = A0;
 const int motorBack = A1;
+bool motorTestActive = false;
 
-bool calibrating = false;
+BLECharacteristic *pCharacteristic;
+BLEServer *pServer;
+bool deviceConnected = false;
+
+bool calibrating = false, calibrated = false;
 unsigned long calibrationStartTime = 0;
-int calibratedMaxValues[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+int calibratedMaxValues[8] = {0};
+int calibratedMinValues[8] = {4095};
+
+const int historySize = 5;
+int pressureHistory[historySize] = {0};
+int historyIndex = 0;
+bool isStationary = false;
+
 
 void turnMotorOn(int motor) {
   digitalWrite(motor, HIGH);
@@ -31,6 +41,72 @@ void turnMotorOn(int motor) {
 
 void turnMotorOff(int motor) {
   digitalWrite(motor, LOW);
+}
+
+void readSensors() {
+  v[0] = analogRead(A2);
+  v[1] = analogRead(A3);
+  v[2] = analogRead(A4);
+  v[3] = analogRead(A5);
+  v[4] = analogRead(A6);
+  v[5] = analogRead(A7);
+  v[6] = analogRead(A8);
+  v[7] = analogRead(A9);
+
+  Serial.println("2\t3\t4\t5\t6\t7\t8\t9");
+    for (int i = 0; i < 8; i++) {
+        Serial.print(v[i]);
+        Serial.print("\t");
+    }
+    Serial.println();
+    Serial.println();
+}
+
+int calculateTotalPressure() {
+  int total = 0;
+  for (int i = 0; i < 8; i++) total += v[i];
+  return total;
+}
+
+bool checkStationary() {
+  int minP = pressureHistory[0], maxP = pressureHistory[0];
+  for (int i = 1; i < historySize; i++) {
+    if (pressureHistory[i] < minP) minP = pressureHistory[i];
+    if (pressureHistory[i] > maxP) maxP = pressureHistory[i];
+  }
+  return (maxP - minP < 100);
+}
+
+void updatePressureHistory(int totalPressure) {
+  pressureHistory[historyIndex] = totalPressure;
+  historyIndex = (historyIndex + 1) % historySize;
+  isStationary = checkStationary();
+}
+
+void sendStringBLE(std::string str) {
+  pCharacteristic->setValue((uint8_t *)str.c_str(), str.length());
+  pCharacteristic->notify();
+}
+
+void updateCalibration() {
+  for (int i = 0; i < 8; i++) {
+    if (v[i] > calibratedMaxValues[i]) calibratedMaxValues[i] = v[i];
+    if (v[i] < calibratedMinValues[i]) calibratedMinValues[i] = v[i];
+  }
+
+  if (millis() - calibrationStartTime >= 5000) {
+    calibrating = false;
+    calibrated  = true;
+
+    Serial.println("Calibration done:");
+    for (int i = 0; i < 8; i++) {
+      Serial.print("Sensor "); Serial.print(i); Serial.print(": Max = ");
+      Serial.print(calibratedMaxValues[i]);
+      Serial.print(", Min = "); Serial.println(calibratedMinValues[i]);
+    }
+  }
+
+  // TODO: send calibrated values to app
 }
 
 class MyCallbacks : public BLECharacteristicCallbacks {
@@ -41,17 +117,21 @@ class MyCallbacks : public BLECharacteristicCallbacks {
             Serial.println("FRONT Motor ON");
             turnMotorOn(motorFront);
             turnMotorOff(motorBack);
+            motorTestActive = true;
         } else if (value == "BACK") {
             Serial.println("BACK Motor ON");
             turnMotorOn(motorBack);
             turnMotorOff(motorFront);
+            motorTestActive = true;
         } else if (value == "OFF") {
             Serial.println("Motor OFF");
             turnMotorOff(motorFront);
             turnMotorOff(motorBack);
+            motorTestActive = false;
         } else if (value == "CALIBRATE") {
           Serial.println("Calibrating!");
           calibrating = true;
+          calibrated = false;
           calibrationStartTime = millis();
           for (int i = 0; i < 8; i++) {
             calibratedMaxValues[i] = 0;
@@ -65,111 +145,106 @@ class MyCallbacks : public BLECharacteristicCallbacks {
     }
 };
 
-class ServerCallbacks : public BLEServerCallbacks {
-    void onConnect(BLEServer* pServer) {
-        deviceConnected = true;
-        Serial.println("Device Connected");
-    }
+void detectLean() {
+  // if (!isStationary) {
+  //   Serial.println("NOT STATIONARY");
+  //   return;
+  // }
 
-    void onDisconnect(BLEServer* pServer) {
-        deviceConnected = false;
-        Serial.println("Device Disconnected, Restarting Advertising...");
-        BLEDevice::startAdvertising();
-    }
+  if (calculateTotalPressure() < 1000) {
+    turnMotorOff(motorFront);
+    turnMotorOff(motorBack);
+  }
+
+  float heelPressure = (v[0] + v[1]) / 2.0;
+  float toePressure = (v[2] + v[4] + v[6]) / 3.0;
+  float leanRatio = toePressure / (heelPressure + toePressure + 1e-5);
+
+  bool toePressed = (v[2] > calibratedMaxValues[2] * 0.6) ||
+                    (v[4] > calibratedMaxValues[4] * 0.6) ||
+                    (v[6] > calibratedMaxValues[6] * 0.6);
+  bool heelDropped = (v[0] < calibratedMaxValues[0] * 0.4) &&
+                     (v[1] < calibratedMaxValues[1] * 0.4);
+
+  bool heelPressed = (v[0] > calibratedMaxValues[0] * 0.9) &&
+                     (v[1] > calibratedMaxValues[1] * 0.9);
+  bool toeDropped = (v[2] < calibratedMaxValues[2] * 0.3) &&
+                    (v[4] < calibratedMaxValues[4] * 0.3) &&
+                    (v[6] < calibratedMaxValues[6] * 0.3);
+
+  if (leanRatio > 0.7 || (toePressed && heelDropped)) {
+    Serial.println("Leaning Forward !!!");
+    turnMotorOn(motorFront);
+    turnMotorOff(motorBack);
+
+    sendStringBLE("Leaning Forward");
+  } else if ((heelPressed && toeDropped)) {
+    Serial.println("Leaning Backward !!!");
+    turnMotorOn(motorBack);
+    turnMotorOff(motorFront);
+
+    sendStringBLE("Leaning Backward");
+  } else {
+    turnMotorOff(motorFront);
+    turnMotorOff(motorBack);
+  }
+}
+
+class ServerCallbacks : public BLEServerCallbacks {
+  void onConnect(BLEServer* pServer) {
+    deviceConnected = true;
+    Serial.println("Device Connected");
+  }
+
+  void onDisconnect(BLEServer* pServer) {
+    deviceConnected = false;
+    Serial.println("Device Disconnected, Restarting Advertising...");
+    BLEDevice::startAdvertising();
+  }
 };
 
 void setup() {
-  // declare the ledPin as an OUTPUT:
   pinMode(motorFront, OUTPUT);
   pinMode(motorBack, OUTPUT);
   Serial.begin(921600);
 
-    BLEDevice::init("ESP32 Motor Controller");
-    pServer = BLEDevice::createServer();
-    pServer->setCallbacks(new ServerCallbacks());
+  BLEDevice::init("ESP32 Motor Controller");
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new ServerCallbacks());
 
-    BLEService *pService = pServer->createService(SERVICE_UUID);
-    pCharacteristic = pService->createCharacteristic(
-        CHARACTERISTIC_UUID,
-        BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_NOTIFY
-    );
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+  pCharacteristic = pService->createCharacteristic(
+      CHARACTERISTIC_UUID,
+      BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_NOTIFY
+  );
 
-    pCharacteristic->setCallbacks(new MyCallbacks());
-    pService->start();
+  pCharacteristic->setCallbacks(new MyCallbacks());
+  pService->start();
 
-    BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
-    pAdvertising->addServiceUUID(SERVICE_UUID);
-    pAdvertising->setScanResponse(true);
-    pAdvertising->setMinPreferred(0x06);
-    pAdvertising->setMinPreferred(0x12);
-    BLEDevice::startAdvertising();
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(true);
+  pAdvertising->setMinPreferred(0x06);
+  pAdvertising->setMinPreferred(0x12);
+  BLEDevice::startAdvertising();
 
-    turnMotorOff(motorFront);
-    turnMotorOn(motorBack);
+  // turnMotorOn(motorFront);
+  // turnMotorOn(motorBack);
 }
 
-
-int cnt = 0;
-int v[8];
 void loop() {
+  readSensors();
 
-   v[0] = analogRead(A2);
-   v[1] = analogRead(A3);
-   v[2] = analogRead(A4);
-   v[3] = analogRead(A5);
-   v[4] = analogRead(A6);
-   v[5] = analogRead(A7);
-   v[6] = analogRead(A8);
-   v[7] = analogRead(A9);
-  // turn the ledPin on
-  Serial.println("2\t3\t4\t5\t6\t7\t8\t9");
-    for (int i = 0; i < 8; i++) {
-        Serial.print(v[i]);
-        Serial.print("\t");
-    }
-    Serial.println();
-    Serial.println();
-
-  if (deviceConnected) {
-    std::string data = "";
-    for (int i =0 ; i< 8; i++) {
-      data += std::to_string(v[i]) + ",";
-    }
-    pCharacteristic->setValue((uint8_t*)data.c_str(), data.length());
-    pCharacteristic->notify();
+  std::string sensorData = "";
+  for (int i = 0; i < 8; i++) {
+    sensorData += std::to_string(v[i]) + ",";
   }
 
-  if (calibrating) {
-    // Update calibration for each sensor channel
-    for (int i = 0; i < 8; i++) {
-      if (v[i] > calibratedMaxValues[i]) {
-        calibratedMaxValues[i] = v[i];
-      }
-    }
+  if (deviceConnected) sendStringBLE(sensorData);
 
-    // Check if 5 seconds have elapsed since calibration started
-    if (millis() - calibrationStartTime >= 5000) {
-      calibrating = false;
-      Serial.println("Calibration Completed. Calibrated max values:");
-      for (int i = 0; i < 8; i++) {
-        Serial.print("Sensor ");
-        Serial.print(i + 2); 
-        Serial.print(": ");
-        Serial.println(calibratedMaxValues[i]);
-      }
-      if (deviceConnected) {
-        std::string calMessage = "CAL_COMPLETE:";
-        for (int i = 0; i < 8; i++) {
-          calMessage += std::to_string(calibratedMaxValues[i]);
-          if (i < 7) {
-            calMessage += ",";
-          }
-        }
-        pCharacteristic->setValue((uint8_t*)calMessage.c_str(), calMessage.length());
-        pCharacteristic->notify();
-      }
-    }
-  }
+  if (calibrating) updateCalibration();
   
+  if (calibrated && !motorTestActive) detectLean();
+
   delay(250);
 }
